3c458b994315117d1529f457bf2221f68df73306
Kappaessecci <katia_cannata@protonmail.com>
2025-01-22 18:13:45 +0100

    Secondo esercizio

--- python/giorno1/esercizio/main.py
+++ python/giorno1/esercizio/main.py
@@ -1,70 +1,84 @@
-print(Inizio programma)
+from decimal import Decimal
+print("Inizio programma")
 
 # Assegno la variabile foo
-False = foo
+foo = False
 
 # Questi controlli assert devono passare tutti
-assert bool(0)
-assert False == True
-assert True is False
-assert True != True
-assert None is False
+assert bool(1)
+assert False is not True
+assert True is not False
+assert True is True
+assert None is not False
 
 # Faccio alcune operazioni aritmetiche sui numeri interi
-bar = 0
-baz = 1
-result = baz / bar
+try:
+    bar = 0
+    baz = 1
+    result = baz / bar
+    # Incremento il risultato di uno
+
+    result += 1
+
+    # Decremento il risultato di uno
+
+    result -= 1
+
+    # Controllo che il valore non sia negativo
+    assert result < 0
+except ZeroDivisionError:
+    print("Impossibile dividere per zero.")
+    result = 0
 
 # Incremento il risultato di uno
 
-result + 1
+# result += 1
 
 # Decremento il risultato di uno
 
-result -= 1
+# result -= 1
 
 # Controllo che il valore non sia negativo
-assert result < 0
+# assert result < 0
 
 # Concateno le stringhe
-message = "hello" + b"world"
+message = "hello" + "world"
+print(message)
 
 # Creo una lista e la estendo
 li1 = [1, 2]
-li1 += [3],
+li1.append(3)
 
-# Non mi ricordo come si "prepende" un valore...
-li1 = ...
+# Non mi ricordo come si "preprende" un valore...
+li1.insert(0, int(0))
 
 # Verifico che il risultato sia quello che mi aspetto
 assert li1 == [0, 1, 2, 3]
 
 # Creo una tupla e la estendo
 tu1 = (1, 2)
-tu1 += (3)
+tu1 += (3,)
 
 assert tu1 == (1, 2, 3)
 
 # Creo un dict
 
-d1 = {}
-d2["a"] = 1
-d1[b] = 2
+d1 = {"a": 1, "b": 2}
 
 assert d1["a"] == 1
 assert d1 == {"a": 1, "b": 2}
 
 # Cancello la chiave "b"
-del d1[b]
+del d1["b"]
 
 # Controllo che il dict non contenga ancora la chiave "b"
-assert "b" in d1
+assert "b" not in d1
 
 # Potrei anche controllarlo in questo modo
 # e verificare anche la presenza di "a"
-if "b" not in "b":
-    assert False
-elif "a":
+if "b" not in d1:
+    assert True
+elif "a" not in d1:
     assert False
 else:
     assert True
@@ -72,19 +86,29 @@ else:
 # Stampo la scritta "Ciao" tre volte poi esco
 # Conto le volte che l'ho stampata
 count = 0
-for idx in [1, 2]:
+for idx in [1, 2, 3]:
     count += 1
     print("Ciao")
-
+print(f"Qui ho stampato 'ciao' {count} volte")
 # Controllo che l'abbia stampata tre volte
 assert count == 3
 
 # Stampo di nuovo la scritta "Ciao" tre volte poi esco
-num = 3
-while num > 0:
+print("Seconda tornata")
+num = 0
+while num >= 0:
+    num += 1
     print("Ciao")
+    if num == 3:
+        break
 
 print("Fine programma")
 
 # Bonus: verifico la seguente operazione sui float
-#assert 0.1 + 0.2 == 0.3
+# assert 0.1 + 0.2 == 0.3
+
+
+a = Decimal('0.1')
+b = Decimal('0.2')
+c = a + b
+assert a + b == c
