e7050ee99b6bf08bc2a0517d01294e2be5e2bab1
Kappaessecci <katia_cannata@protonmail.com>
2025-01-29 16:45:13 +0100

    Esercizio rubrica

--- python/giorno4/main.py
+++ python/giorno4/main.py
@@ -1,1 +1,29 @@
-# Implement qui il codice
+# Implementa qui il codice
+
+class Contact:
+  def __init__(self, name, surname, phone=None):
+    self.name = name
+    self.phone = phone
+    self.surname = surname
+
+class Person(Contact):
+  pass
+
+class Business(Contact):
+  pass
+
+class Directory:
+  def __init__(self):
+    self.contacts = []
+
+  def __len__(self):
+    return len(self.contacts)
+
+  def add(self, contact):
+    self.contacts.append(contact)
+
+  def query(self, name):
+    return [contact for contact in self.contacts if contact.name == name]
+
+  def find(self, search_term):
+    return [contact for contact in self.contacts if search_term in contact.name or (contact.phone and search_term in contact.phone)]
