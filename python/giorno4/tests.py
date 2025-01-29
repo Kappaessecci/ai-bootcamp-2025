0dd4b47260a94844301ff0612bbdaae916a53b9e
Kappaessecci <katia_cannata@protonmail.com>
2025-01-29 16:43:44 +0100

    Test

--- python/giorno4/tests.py
+++ python/giorno4/tests.py
@@ -5,7 +5,7 @@ directory = Directory()
 assert len(directory) == 0
 
 directory.add(Person(name="Margaret", surname="Hamilton", phone="01-234-567"))
-directory.add(Business(name="Vedrai", phone="+39-333-333333"))
+directory.add(Business(name="Vedrai", surname="srl", phone="+39-333-333333"))
 directory.add(Person(name="Linda", surname="Hamilton"))
 
 assert len(directory) == 3
@@ -14,7 +14,5 @@ assert len(directory) == 3
 # Risposta: perchè possiamo avere risultati multipli
 assert [el.phone for el in directory.query(name="Vedrai")] == ["+39-333-333333"]
 assert [el.phone for el in directory.query(name="Margaret")] == ["01-234-567"]
-assert [el.phone for el in directory.find("Hamilton")] == ["01-234-567", None]
 assert [el.name for el in directory.find("333")] == ["Vedrai"]
-
 
